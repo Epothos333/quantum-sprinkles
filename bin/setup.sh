711ee8a231beb3c554ab93e25c0dd63ac43a9377
Johnson Denen <johnson.denen@gmail.com>
2016-05-11 14:39:43 -0400

    Tweak setup script to install rbenv and ruby 2.2.2

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+# install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+~/.rbenv/bin/rbenv init
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
+# source profile so we can use rbenv
+. ~/.bash_profile
+
+# install ruby 2.2.2 as global version
+rbenv install 2.2.2
+rbenv global 2.2.2
+
+# project setup
 gem install bundler
 bundle install
+
+# rbenv shims
+rbenv rehash
