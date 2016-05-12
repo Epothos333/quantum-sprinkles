24703f3bab3f55a836a036d533d2616f3ca4469f
David Edward Weirich <dweirich@pillartechnology.com>
2016-05-12 15:58:10 -0400

    First iteration of calling Jazz

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'rest-client'
 
 get '/' do
-  'Quantum Sprinkles'
+
+  json_result = RestClient.get("https://api.resumatorapi.com/v1/applicants?apikey=D9PNCCQl8ZtdF7zjtGksn2h4x2jZltP5")
+  candidate_count = JSON.parse(json_result).length
+
+  "Quantum Sprinkles, Total: #{candidate_count}"
 end
+
--- spec/quantum_sprinkles_spec.rb
+++ spec/quantum_sprinkles_spec.rb
@@ -1,9 +1,41 @@
 require File.expand_path '../spec_helper.rb', __FILE__
+require 'rest-client'
 
 describe 'Quantum Sprinkles Application' do
   it 'should show quantum sprinkles' do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq 'Quantum Sprinkles'
+    expect(last_response.body).to include 'Quantum Sprinkles'
+  end
+
+  it 'should show the count from Jazz' do
+
+    json_result = '[
+      {
+        "id": "prospect_20160512184407_MYXVB4IKB2IALSSA",
+        "first_name": "Alan",
+        "last_name": "Turing",
+        "prospect_phone": "2345678910",
+        "apply_date": "2016-05-12",
+        "job_id": "job_20160512181416_C2ZAFIUBO61DKFDA",
+        "job_title": "Software Craftsman Developer"
+      },
+      {
+        "id": "prospect_20160512182822_TKPWIHRLRGFGEQCU",
+        "first_name": "Harry",
+        "last_name": "Potter",
+        "prospect_phone": "2345678910",
+        "apply_date": "2016-05-12",
+        "job_id": "job_20160512181011_4ZHR7DSN1ZIHLYSQ",
+        "job_title": "Software Journeyman Developer"
+    }]'
+
+#    rest_double = class_double(RestClient)
+    expect(RestClient).to receive(:get) { json_result }
+
+    candidate_count = 2
+    # setup a fake api that returns 2 candidates
+    get '/'
+    expect(last_response.body).to include "Total: #{candidate_count}"
   end
 end
