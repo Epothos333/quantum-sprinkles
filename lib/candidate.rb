53ea289b40fe4155452afb7de4a0f83d164d6371
David Edward Weirich <dweirich@pillartechnology.com>
2016-05-12 16:13:54 -0400

    Refactored to use constants for api

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,6 +1,10 @@
 class Candidate
+
+  API_KEY = 'D9PNCCQl8ZtdF7zjtGksn2h4x2jZltP5'
+  API_BASE_URL = 'https://api.resumatorapi.com/v1/'
+
   def self.total
-    json_result = RestClient.get("https://api.resumatorapi.com/v1/applicants?apikey=D9PNCCQl8ZtdF7zjtGksn2h4x2jZltP5")
+    json_result = RestClient.get("#{API_KEY}applicants?apikey=#{API_BASE_URL}")
     JSON.parse(json_result).length
   end
 end
