ff1d3930ba4dd46382a542d97dabb3a31e5000ec
Edwin Aoraha <edwinaoraha@Edwins-MacBook-Pro.local>
2016-05-13 12:48:12 -0400

    Fixed rake errors. Make only 1 call to server

--- lib/app.rb
+++ lib/app.rb
@@ -3,12 +3,13 @@ require 'rest-client'
 require_relative 'candidate'
 
 get '/' do
-  @candidate_count = Candidate.total
-  @artisan_count = Candidate.get_family_totals["artisans"]
-  @delivery_count = Candidate.get_family_totals["delivery"]
-  @ux_count = Candidate.get_family_totals["ux"]
-  @consultant_count = Candidate.get_family_totals["consultants"]
-  @devops_count = Candidate.get_family_totals["devops"]
+  @family_totals = Candidate.get_family_totals
+  @artisan_count = @family_totals["artisans"]
+  @delivery_count = @family_totals["delivery"]
+  @ux_count = @family_totals["ux"]
+  @consultant_count = @family_totals["consultants"]
+  @devops_count = @family_totals["devops"]
+  @candidate_count = @artisan_count + @delivery_count + @ux_count + @consultant_count + @devops_count
   erb :index
 end
 
