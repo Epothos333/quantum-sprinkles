d445ed543bfa7e09a03d1cc07fb9ff12b103cea4
David Edward Weirich <dweirich@pillartechnology.com>
2016-05-13 10:21:33 -0400

    Artians test passing

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path '../spec_helper.rb', __FILE__
+require 'rest-client'
+
+describe 'Candidate' do
+  before(:each) do
+    json_result = '[
+      {
+        "id": "prospect_20160512184407_MYXVB4IKB2IALSSA",
+        "first_name": "Alan",
+        "last_name": "Turing",
+        "prospect_phone": "2345678910",
+        "apply_date": "2016-05-12",
+        "job_id": "job_20160512181416_C2ZAFIUBO61DKFDA",
+        "job_title": "Software Craftsman Developer"
+      },
+      {
+        "id": "prospect_20160512182822_TKPWIHRLRGFGEQCU",
+        "first_name": "Harry",
+        "last_name": "Potter",
+        "prospect_phone": "2345678910",
+        "apply_date": "2016-05-12",
+        "job_id": "job_20160512181011_4ZHR7DSN1ZIHLYSQ",
+        "job_title": "Software Journeyman Developer"
+    }]'
+#    expect(RestClient).to receive(:get) { json_result }
+  end
+
+  it 'should return totals by job family' do
+    family_totals = Candidate.get_family_totals
+    expect(family_totals["artisans"]).to eq 2
+  end
+
+end
